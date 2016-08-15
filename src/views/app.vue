b1ce06e79dcc1f52b5eca44af94a4b010600cdb6
Allen Tong <sleepyxivo@gmail.com>
2016-08-14 21:58:59 -0700

    Closes issue 18 - pace.js loader

--- src/views/app.vue
+++ src/views/app.vue
@@ -1,8 +1,3 @@
-<style lang="less">
- @import '../../node_modules/bootstrap/less/bootstrap.less';
- @import '../assets/stylesheets/style.less';
-</style>
-
 <template>
   <div id="app" class="wrapper">
       <router-view></router-view>
@@ -10,15 +5,19 @@
 </template>
 
 <script>
-// import pace from 'pace';
+import 'bootstrap/less/bootstrap.less';
+import 'font-awesome/css/font-awesome.css';
+import '../assets/stylesheets/style.less';
+
+import Pace from 'pace-progress';
 import Auth from '../services/auth.js';
 
 class App {
   constructor() {
     this.auth = new Auth();
+    Pace.start();
   }
 }
-// pace.start();
 
 export default new App;
 </script>
