93aac568e55bfb43a72b18b4460052ff54bcf01d
Allen Tong <sleepyxivo@gmail.com>
2016-08-15 21:26:37 -0700

    Closes issue #16 Derp - add component search to site

---
+++ src/components/productSearch/productSearch.vue
@@ -0,0 +1,45 @@
+<template>
+  <form>
+    <div class="form-group">
+      <input class="form-control" type="text" placeholder="Search for a product" autocomplete="false">
+    </div>
+  </form>
+</template>
+<script>
+  export default {
+    // Options / Data
+    data() {
+      return {};
+    },
+    props: [],
+    computed: {},
+    methods: {},
+    // watch: {},
+    // Options / DOM
+    // el () {},
+    // replace: true,
+    // template: '',
+    // Options / Lifecycle Hooks
+    // init () {},
+    // crated () {},
+    // beforeCompile () {},
+    // compiled () {},
+    // ready () {},
+    // attached () {},
+    // detached () {},
+    // beforeDestroy () {},
+    // destroyed () {},
+    // Options / Assets
+    // directives: {},
+    // elementDirectives: {},
+    // filters: {},
+    // components: {},
+    // transitions: {},
+    // partials: {},
+    // Options / Misc
+    // parent: null,
+    // events: {},
+    // mixins: [],
+    // name: ''
+  };
+</script>
