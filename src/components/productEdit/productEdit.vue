ede9b5fe7f1529d3085f73698cbd0363b6baabe0
Allen Tong <sleepyxivo@gmail.com>
2016-09-22 23:11:38 -0700

    feat(style): style edit products page

--- src/components/productEdit/productEdit.vue
+++ src/components/productEdit/productEdit.vue
@@ -1,78 +1,118 @@
 <template>
   <div class="row control-row">
-    <div class="col-md-12 text-right">
-      <button class="btn btn-primary" v-on:click="save()">Save</button>
+    <div class="col-md-12">
+      <button class="btn btn-primary pull-right" v-on:click="save()">Save</button>
+      <h4>Product Details</h4>
     </div>
   </div>
-  <div class="panel panel-filled">
-      <div class="panel-body">
-          <form>
-              <div class="row">
-                  <div class="col-md-12">
-                      <div class="form-group">
-                          <label>Product Title</label>
-                          <input type="text" class="form-control" placeholder="Enter a title..." v-model="product.title">
+  <div class="panel panel-filled panel-main">
+    <div class="panel-body">
+      <form>
+        <div class="media">
+          <div class="media-left">
+            <a href="#">
+            <img class="media-object" src="http://placehold.it/250x250">
+            </a>
+          </div>
+          <div class="media-body">
+            <div class="form-group">
+              <label>Product Title</label>
+              <input type="text" class="form-control" placeholder="Enter a title..." v-model="product.title">
+            </div>
+            <div class="form-group">
+              <label>Product Description</label>
+              <textarea class="form-control" placeholder="Enter a description..." v-model="product.description"></textarea>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+          </div>
+          <div class="col-md-12">
+            <div class="tabs-container">
+              <tabs>
+                <tab header="Variants">
+                  <div class="clearfix">
+                    <div class="panel panel-filled" v-for="variant in product.variants">
+                      <div class="panel-heading">
+                        {{variant.title}}
                       </div>
-                      <div class="form-group">
-                          <label>Product Description</label>
-                          <textarea class="form-control" placeholder="Enter a description..." v-model="product.description"></textarea
+                      <div class="panel-body">
+                        <div class="col-md-4">
+                          <div class="form-group">
+                            <label>Price (USD)</label>
+                            <input type="text" class="form-control" placeholder="Price" v-model="variant.price">
+                          </div>
+                        </div>
+                        <div class="col-md-4">
+                          <div class="form-group">
+                            <label>SKU</label>
+                            <input type="text" class="form-control" placeholder="SKU" v-model="variant.sku">
+                          </div>
+                        </div>
+                        <div class="col-md-4">
+                          <div class="form-group">
+                            <label>Weight (Unit: {{ variant.weightUnit }})</label>
+                            <input type="text" class="form-control" placeholder="Weight" v-model="variant.weight">
+                          </div>
+                        </div>
                       </div>
+                    </div>
+                    <button class="btn btn-accent pull-right" v-on:click.prevent="addVariant()">Add Variant</button>
                   </div>
-              </div>
-              <div class="row">
-                  <div class="col-md-12">
-                      <h5>Product Details</h5>
-                  </div>
-                  <div class="col-md-12">
-                      <div class="tabs-container">
-                          <tabs>
-                              <tab header="Variants">
-                                  <div class="panel-body">
-                                    <div class="row" v-for="variant in product.variants">
-                                      <div class="col-md-12">
-                                        <div class="form-group">
-                                          <label>{{ variant.title}}</label>
-                                          <input type="text" class="form-control" placeholder="title" v-model="variant.title">
-                                        </div>
-                                        <div class="form-group">
-                                          <label>Price</label>
-                                          <input type="text" class="form-control" placeholder="title" v-model="variant.price">
-                                        </div>
-                                        <div class="form-group">
-                                          <label>SKU</label>
-                                          <input type="text" class="form-control" placeholder="title" v-model="variant.sku">
-                                        </div>
-                                        <div class="form-group">
-                                          <label>Weight (Unit: {{ variant.weightUnit }})</label>
-                                          <input type="text" class="form-control" placeholder="title" v-model="variant.weight">
-                                        </div>
-                                      </div>
-                                  </div>
-                                </div>
-                              </tab>
-                              <tab header="Warehouses">
-                                  <div class="panel-body">
-                                  </div>
-                              </tab>
-                          </tabs>
-                      </div>
-                  </div>
-              </div>
-          </form>
-      </div>
+                </tab>
+              </tabs>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
-<style scope>
-  textarea {
+<style lang="less" scope>
+  .panel-main {
+    padding-top: 15px;
+  }
+  .tabs-container {
+    margin-top: 20px;
+  }
+  textarea.form-control {
     resize: none;
-    height: 200px;
+    height: 152px;
   }
   .nav-tabs {
     margin-bottom: 0 !important;
   }
   .control-row {
     margin-bottom: 20px;
+  }
+
+  .tab-content {
+    margin-top: 1px;
+  }
+  .tab-pane {
+    padding: 10px 15px;
+    background-color: rgba(68, 70, 79, 0.5);
+
+    .panel {
+      &:first-child {
+        margin-top: 4px;
+      }
+      background-color: transparent;;
+      border: 1px solid rgba(116, 124, 158, 0.2);
+
+      .panel-filled {
+        background-color: transparent;;
+      }
+      .panel-heading {
+        background-color: transparent;
+        font-weight: bold;
+      }
+      .panel-body {
+        background-color: transparent;
+      }
+    }
   }
 </style>
 
@@ -110,6 +150,11 @@
         // .then(json => {
         //   // console.log(json);
         // });
+      },
+      addVariant() {
+        this.product.variants.push({
+          weightUnit: 'lb',
+        });
       },
     },
     ready() {
