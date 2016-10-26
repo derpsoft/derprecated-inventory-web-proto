b997d7cb980dba70b5473c13420dc353d3fb1edc
Allen Tong <sleepyxivo@gmail.com>
2016-10-26 11:34:26 -0700

    chore(derp) - redirect all express routes

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-var express = require('express'); 
+var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/dist'));
+
+app.get('*', function (req, res) {
+  res.send('root');
+});
 
 app.listen(process.env.PORT || 3000);
