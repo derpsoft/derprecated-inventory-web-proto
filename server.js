63334cc25a02d97761f0b773bad8ad113908f66e
Allen Tong <sleepyxivo@gmail.com>
2016-10-26 12:44:44 -0700

    chore(derp) - back to history

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ var express = require('express');
 var app = express();
 var history = require('connect-history-api-fallback');
 
-var hist = history({
-  rewrites: [
-    { from: '*', to: '/index.html'}
-  ]
-});
+// var hist = history({
+//   rewrites: [
+//     { from: */, to: '/index.html'}
+//   ]
+// });
 
 app.use(express.static(__dirname + '/dist'));
-app.use(hist);
+app.use(history());
 app.listen(process.env.PORT || 3000);
