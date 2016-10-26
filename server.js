bc66f1e4aff1c695f169569efdaee0b1ae8a22b6
Allen Tong <sleepyxivo@gmail.com>
2016-10-26 11:39:11 -0700

    chore(route) - update route -- testing

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.static(__dirname + '/dist'));
 
 app.get('*', function (req, res) {
-  res.send('root');
+  res.send('/');
 });
 
 app.listen(process.env.PORT || 3000);
