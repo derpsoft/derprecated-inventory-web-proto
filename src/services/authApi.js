f40cd1d0653bd9f14b2542b41264a1c3b4b1ddf3
James Cunningham <james@bauscode.com>
2016-11-14 11:39:48 -0700

    fix(DERP) login POST issue with content type

--- src/services/authApi.js
+++ src/services/authApi.js
@@ -32,12 +32,11 @@ class AuthApi extends Fetchable {
   }
 
   login(username, password) {
-    const creds = {
-      username,
-      password
-    };
     return super.post('/auth/credentials', {
-      body: JSON.stringify(creds)
+      body: this.toForm({
+        username,
+        password
+      })
     })
     .then(res => res.json())
     .then(json => {
