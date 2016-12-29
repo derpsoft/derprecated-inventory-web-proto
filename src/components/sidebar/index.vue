51fa60ee1ae3487f488f024ea5cd8196193b396f
Allen Tong <sleepyxivo@gmail.com>
2016-12-28 16:07:01 -0800

    fix(router): resolve issue with active links

--- src/components/sidebar/index.vue
+++ src/components/sidebar/index.vue
@@ -6,40 +6,40 @@
         General
       </li>
       <li>
-        <router-link :to="{ path: '/', activeClass: 'active', exact: true}">Dashboard</router-link>
+        <router-link :to="{ path: '/', activeClass: 'active', exact: true}" active-class="active" exact="true">Dashboard</router-link>
       </li>
       <li>
-        <router-link :to="{ path: '/inventory', activeClass: 'active', exact: true }">Inventory</router-link>
+        <router-link :to="{ path: '/inventory', activeClass: 'active', exact: true }" active-class="active">Inventory</router-link>
       </li>
       <li>
-        <router-link :to="{ path: '/reports', activeClass: 'active', exact: true }">Reports</router-link>
+        <router-link :to="{ path: '/reports', activeClass: 'active', exact: true }" active-class="active">Reports</router-link>
       </li>
       <li class="nav-category">
         Admin
       </li>
       <li>
-        <router-link :to="{ path: '/products', activeClass: 'active'}">Products</router-link>
+        <router-link :to="{ path: '/products', activeClass: 'active'}" active-class="active">Products</router-link>
       </li>
       <li>
-        <router-link :to="{ path: '/categories', activeClass: 'active'}">Categories</router-link>
+        <router-link :to="{ path: '/categories', activeClass: 'active'}" active-class="active">Categories</router-link>
       </li>
       <li>
-        <router-link :to="{ path: '/warehouses', activeClass: 'active'}">Warehouses</router-link>
+        <router-link :to="{ path: '/warehouses', activeClass: 'active'}" active-class="active">Warehouses</router-link>
       </li>
       <li>
-        <router-link :to="{ path: '/vendors', activeClass: 'active'}">Vendors</router-link>
+        <router-link :to="{ path: '/vendors', activeClass: 'active'}" active-class="active">Vendors</router-link>
       </li>
       <li>
-        <router-link :to="{ path: '/users', activeClass: 'active'}">Users</router-link>
+        <router-link :to="{ path: '/users', activeClass: 'active'}" active-class="active">Users</router-link>
       </li>
       <li class="nav-category">
         Account
       </li>
       <li>
-        <router-link :to="{ path: '/profile', activeClass: 'active'}">Profile</router-link>
+        <router-link :to="{ path: '/profile', activeClass: 'active'}" active-class="active">Profile</router-link>
       </li>
       <li>
-        <router-link :to="{ path: '/logout', activeClass: 'active'}">Logout</router-link>
+        <router-link :to="{ path: '/logout', activeClass: 'active'}" active-class="active">Logout</router-link>
       </li>
       <li class="nav-info">
         <i class="pe pe-7s-shield text-accent"></i>
