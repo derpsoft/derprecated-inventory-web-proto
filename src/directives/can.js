9b2feb293d68318464dcda8b883939632106e8f2
James Cunningham <james@bauscode.com>
2016-12-30 18:34:25 -0700

    fix(DERP) permissions and profile load on logout/login

--- src/directives/can.js
+++ src/directives/can.js
@@ -17,13 +17,20 @@ const guards = [
   'canUpsertCategories',
 ];
 
+const toggleHide = (el, shown = true) => {
+  if (shown && el.classList.contains('hide')) {
+    el.classList.remove('hide');
+  } else if (!el.classList.contains('hide')) {
+    el.classList.add('hide');
+  }
+};
+
 export default (Vue) => {
   guards.forEach((v) => {
-    Vue.directive(v, (el) => {
-      if (!store.getters[v] && !el.classList.contains('hide')) {
-        el.classList.add('hide');
-      } else if (el.classList.contains('hide')) {
-        el.classList.remove('hide');
+    Vue.directive(v, {
+      bind: (el) => {
+        toggleHide(el, store.getters[v]);
+        store.watch(() => store.getters[v], current => toggleHide(el, current));
       }
     });
   });
