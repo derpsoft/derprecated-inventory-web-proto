8d2b8680729ec4ed80909bb58c25b0e4ea46cde1
James Cunningham <james@bauscode.com>
2017-01-05 13:07:13 -0700

    fix(#107) add autocomplete for warehouse id

--- src/components/locations/add.vue
+++ src/components/locations/add.vue
@@ -11,6 +11,17 @@
       <form>
         <div class="media">
           <div class="form-group">
+            <label>Warehouse</label>
+
+            <autocomplete
+            :suggestions="warehouses"
+            :key-selector="(v) => `${v.name}`"
+            :value-selector="(v) => v"
+            :display-selector="(v) => `${v.id}: ${v.name}`"
+            @change="setWarehouse">
+          </autocomplete>
+        </div>
+          <div class="form-group">
             <label>Name</label>
             <input type="text" class="form-control" placeholder="Name" v-model="location.name">
           </div>
@@ -24,20 +35,35 @@
 <script>
 import Constants from '../../constants';
 import store from '../../stores/store';
+import Autocomplete from '../autocomplete.vue';
 
 export default {
+  components: { Autocomplete },
   data() {
     return {
       location: {},
+      warehouse: {},
     };
+  },
+  computed: {
+    warehouses() {
+      return store.getters.warehouseList;
+    }
+  },
+  mounted() {
+    store.dispatch(Constants.GET_WAREHOUSES, { skip: 0, take: 1000 });
   },
   methods: {
     save() {
       const location = JSON.parse(JSON.stringify(this.location));
-      location.id = this.id;
+      location.warehouseId = this.warehouse.id;
+
       store.dispatch(Constants.CREATE_LOCATION, {
         location
       });
+    },
+    setWarehouse(w) {
+      this.warehouse = w;
     }
   }
 };
