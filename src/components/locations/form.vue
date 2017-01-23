75d30c76dd3bea6dc6c3de2f64cd7fc030442f03
James Cunningham <james@bauscode.com>
2017-01-23 12:35:16 -0700

    chore(DERP) add location fields and remove name requirement

--- src/components/locations/form.vue
+++ src/components/locations/form.vue
@@ -1,24 +1,44 @@
 <template>
   <form @submit.prevent="validate">
+    <div class="form-group">
+      <label>Warehouse</label>
+
+      <autocomplete
+      :suggestions="warehouses"
+      :key-selector="(v) => `${v.name}`"
+      :value-selector="(v) => v"
+      :display-selector="(v) => `${v.id}: ${v.name}`"
+      @change="setWarehouse"
+      ></autocomplete>
+    </div>
     <div class="form-group" :class="{'has-error': errors.has('name')}">
       <label>Name</label>
       <input type="text" class="form-control" placeholder="Location Name" name="name"
         v-model="value.name"
         v-validate.initial="value.name"
-        data-vv-rules="required"
         @change="change">
-        <span v-show="errors.has('name')" class="help-block">{{ errors.first('name') }}</span>
+      <span v-show="errors.has('name')" class="help-block">{{ errors.first('name') }}</span>
     </div>
-    <div class="form-group">
-      <label>Warehouse</label>
-
-      <autocomplete
-        :suggestions="warehouses"
-        :key-selector="(v) => `${v.name}`"
-        :value-selector="(v) => v"
-        :display-selector="(v) => `${v.id}: ${v.name}`"
-        @change="setWarehouse"
-        ></autocomplete>
+    <div class="form-group" :class="{'has-error': errors.has('shelf')}">
+      <label>Shelf</label>
+      <input type="text" class="form-control" placeholder="Shelf" name="shelf"
+        v-model="value.shelf"
+        @change="change">
+        <span v-show="errors.has('shelf')" class="help-block">{{ errors.first('shelf') }}</span>
+    </div>
+    <div class="form-group" :class="{'has-error': errors.has('rack')}">
+      <label>Rack</label>
+      <input type="text" class="form-control" placeholder="Rack" name="rack"
+        v-model="value.rack"
+        @change="change">
+        <span v-show="errors.has('rack')" class="help-block">{{ errors.first('rack') }}</span>
+    </div>
+    <div class="form-group" :class="{'has-error': errors.has('bin')}">
+      <label>Bin</label>
+      <input type="text" class="form-control" placeholder="Bin" name="bin"
+        v-model="value.bin"
+        @change="change">
+        <span v-show="errors.has('bin')" class="help-block">{{ errors.first('bin') }}</span>
     </div>
   </form>
 </template>
