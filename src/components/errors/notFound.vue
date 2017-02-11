95315ed5812a3716bb8f999fdd9226ee14b45ead
Allen Tong <sleepyxivo@gmail.com>
2017-02-11 11:15:16 -0800

    feat(derp): update 404 to redirect to wrapper

--- src/components/errors/notFound.vue
+++ src/components/errors/notFound.vue
@@ -1,16 +1,27 @@
 <template>
 <section class="content">
-  <div class="container-center">
+  <div class="container-center md animated slideInDown">
     <div class="view-header">
       <div class="header-icon">
-        <i class="pe page-header-icon pe-7s-unlock"></i>
+        <i class="pe page-header-icon pe-7s-close-circle"></i>
       </div>
       <div class="header-title">
-        <h3>Not Found</h3>
+        <h3>404</h3>
         <small>
-            404 Not Found
-        </small>
+        Page Not Found!
+      </small>
       </div>
+    </div>
+
+    <div class="panel panel-filled panel-c-warning">
+      <div class="panel-body text-warning">
+        <h2><i class="fa fa-warning"></i> OH NO!  YOU'VE DERPED. </h2>
+        <p>This page was not found.</p>
+        <p>Return to safety by clicking the button below.</p>
+      </div>
+    </div>
+    <div>
+      <router-link :to="{path: '/'}" class="btn btn-accent">Back to app</router-link>
     </div>
   </div>
 </section>
