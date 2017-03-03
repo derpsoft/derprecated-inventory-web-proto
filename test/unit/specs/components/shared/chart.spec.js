9ca626972c527a582ab0432a69bc3f70b5e10b82
Allen Tong <sleepyxivo@gmail.com>
2017-03-02 18:04:58 -0800

    chore(lint): missed a comma

--- test/unit/specs/components/shared/chart.spec.js
+++ test/unit/specs/components/shared/chart.spec.js
@@ -58,7 +58,7 @@ describe('Chart - Shared Component', () => {
       series: [
         [12, 9, 7, 8, 5],
         [2, 1, 3.5, 7, 3],
-        [1, 3, 4, 5, 6]
+        [1, 3, 4, 5, 6],
       ],
       options: {},
     };
