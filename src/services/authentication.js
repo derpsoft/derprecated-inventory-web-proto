c43f0f66a60107e1db2b295fd1b77693402c6c06
James Cunningham <james@bauscode.com>
2017-03-03 19:26:37 -0700

    feat(DERP) finish auth0 convert, adjust paging

--- src/services/authentication.js
+++ src/services/authentication.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import Auth0Lock from 'auth0-lock';
 import config from 'src/../config';
 
@@ -8,42 +9,57 @@ const {
 const options = {
   auth: {
     redirect: true,
+    params: {
+      // audience: config.auth0.audience,
+      scope: config.auth0.scope,
+    },
   }
 };
 
+
+const _getUserInfo = _.throttle((lock, {
+  accessToken,
+  idToken,
+}) => {
+  return new Promise((resolve, reject) => {
+    lock.getUserInfo(accessToken, (error, profile) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve({
+        profile: {
+          ...profile,
+          userName: profile.nickname,
+          displayName: profile.name,
+        },
+        accessToken,
+        idToken,
+      });
+    });
+  });
+}, 5000, {
+  leading: true,
+  trailing: false
+});
+
+/*
+  Wrapper for auth0 lock calls and similar.
+
+  See https://auth0.com/docs/architecture-scenarios/application/spa-api
+  for a discussion about structure and explanation of the keys in use.
+ */
 class Auth {
   constructor() {
     this.lock = new Auth0Lock(clientId, domain, options);
   }
 
-  getUserInfo({
-    accessToken,
-    idToken,
-  }) {
-    return new Promise((resolve, reject) => {
-      this.lock.getUserInfo(accessToken, (error, profile) => {
-        if (error) {
-          return reject(error);
-        }
-        return resolve({
-          profile: {
-            ...profile,
-            userName: profile.nickname,
-            displayName: profile.name,
-          },
-          permissions: '',
-          accessToken,
-          idToken,
-        });
-      });
-    });
+  getUserInfo(tokens) {
+    return _getUserInfo(this.lock, tokens);
   }
 
   onAuthenticated(callback) {
-    this.lock.on('authenticated', ({
-      accessToken
-    }) => {
-      this.getUserInfo(accessToken)
+    this.lock.on('authenticated', (authResult) => {
+      this.getUserInfo(authResult)
         .then(callback)
         .catch((e) => {
           throw e;
