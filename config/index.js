d6d9ea0f622b58346ddd5d7890fb8f3bf45c9e92
Allen Tong <sleepyxivo@gmail.com>
2017-05-22 20:43:01 -0700

    revert index.js

--- config/index.js
+++ config/index.js
@@ -55,8 +55,7 @@ const sta = _.merge({}, pro, {
 });
 
 const local = _.merge({}, base, {
-  // api: 'http://0.0.0.0:5000',
-  api: 'https://inventory-api-dev.azurewebsites.net',
+  api: 'http://0.0.0.0:5000',
   autoOpenBrowser: true,
   cssSourceMap: false,
 });
