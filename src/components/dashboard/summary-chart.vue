8846378ac91833e03704572da5cdafc7235beac9
Allen Tong <sleepyxivo@gmail.com>
2017-05-23 20:20:21 -0700

    wip(chart): chartjs

--- src/components/dashboard/summary-chart.vue
+++ src/components/dashboard/summary-chart.vue
@@ -14,6 +14,12 @@ export default {
   name: 'summary-chart',
   data () {
     return {
+      label: null,
+      series: {
+        dispatched: [0],
+        received: [0],
+        sales: [0]
+      },
       chartJsData: null,
       chartJsOptions: {
         responsive: true,
@@ -25,25 +31,55 @@ export default {
     LineChart,
   },
   computed: {
-    labels() {
-      const fmt = x => moment.utc(x).format('LL');
-      return [
-        _.map(this.$store.getters.dashboard.received.labels, fmt),
-        _.map(this.$store.getters.dashboard.dispatched.labels, fmt),
-        _.map(this.$store.getters.dashboard.sales.labels, fmt),
-      ];
-    },
-    series() {
-      return [
-        this.$store.getters.dashboard.received.series,
-        this.$store.getters.dashboard.dispatched.series,
-        this.$store.getters.dashboard.sales.series
-      ];
+    dashboardSeries() {
+      return JSON.parse(JSON.stringify(this.$store.getters.dashboard));
     },
   },
+  watch: {
+    dashboardSeries: 'loadChart'
+  },
   methods: {
+    buildSeries(list, dataset) {
+      const arr = [];
+      let index = -1;
+
+      list.forEach((value) => {
+        index = dataset.labels.indexOf(value);
+
+        if (index > -1) {
+          arr.push(dataset.series[index]);
+        } else {
+          arr.push(0);
+        }
+      });
+
+      return arr;
+    },
+    createLabels(labels) {
+      const fmt = x => moment.utc(x).format('LL');
+      return _.map(labels, fmt);
+    },
+    createDataKeyList(labels) {
+      return _.map(_.sortBy(_.uniq(labels)));
+    },
+    loadChart() {
+      const dataList = this.createDataKeyList([
+        ...this.dashboardSeries.received.labels,
+        ...this.dashboardSeries.dispatched.labels,
+        ...this.dashboardSeries.sales.labels
+      ]);
+
+      this.series.received = this.buildSeries(dataList, this.dashboardSeries.received);
+      this.series.dispatched = this.buildSeries(dataList, this.dashboardSeries.dispatched);
+      this.series.sales = this.buildSeries(dataList, this.dashboardSeries.sales);
+
+      this.labels = this.createLabels(dataList);
+
+      this.renderSummary();
+    },
     renderSummary() {
       this.chartJsData = {
+        labels: this.labels,
         datasets: [
           {
             label: 'Total Sales',
@@ -52,7 +88,7 @@ export default {
             borderWidth: 1,
             pointBorderColor: 'white',
             backgroundColor: 'rgba(247, 175, 62, 0.5)',
-            data: this.series[2]
+            data: this.series.sales
           }, {
             label: 'Total Received',
             borderColor: 'rgba(54, 177, 222, 1)',
@@ -60,7 +96,7 @@ export default {
             pointBorderColor: 'white',
             borderWidth: 1,
             backgroundColor: 'rgba(54, 177, 222, 0.5)',
-            data: this.series[0]
+            data: this.series.received
           }, {
             label: 'Total Dispatched',
             borderColor: 'rgba(27, 191, 137, 1)',
@@ -68,7 +104,7 @@ export default {
             pointBorderColor: 'white',
             borderWidth: 1,
             backgroundColor: 'rgba(27, 191, 137, 0.5)',
-            data: this.series[1]
+            data: this.series.dispatched
           }
         ],
       };
